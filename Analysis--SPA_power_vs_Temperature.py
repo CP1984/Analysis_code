9df3aa55c448452a4510b235ec5d97febfb4bc2a
CP1984 <chingping1984@gmail.com>
2024-03-18 14:53:24 +0900

    change the average in Watt unit, not Voltage unit

--- Analysis--SPA_power_vs_Temperature.py
+++ Analysis--SPA_power_vs_Temperature.py
@@ -11,7 +11,7 @@ from tkinter import filedialog
 #    Analysis parameters     #
 
 # Define the folder path where you save the original data
-folder_path = "D:\\NAS\\temporary Storage Zone\\OIST\\Data\\Noise_temperature_measurement\\2024\\03\\temp\\"
+folder_path = "D:\\Cloud\\temporary Storage Zone\\OIST\\Data\\Noise_temperature_measurement\\2024\\03\\temp\\"
 save_data = False  # Set to True to save data in HDF5 format; set to False to plot data using pyplot
 
 key_word = "mK" # This keyword indicates the unit used in the file names
@@ -49,21 +49,21 @@ for file_name in file_list:
     amp_dBm = f.getData('SPA data')
 
     # Calculate the average amplitude
-    amp_Vrms = np.sqrt((50/1000) * (10**(amp_dBm/10)))
-    error_Vrms = np.std(amp_Vrms) 
-    avg_amp_Vrms = np.average(amp_Vrms)
-    avg_amp_dBm = 10 * np.log10(1000*(avg_amp_Vrms**2)/50)
+    amp_Watt = (1/1000) * (10**(amp_dBm/10))
+    error_Watt = np.std(amp_Watt) 
+    avg_amp_Watt = np.average(amp_Watt)
+    avg_amp_dBm = 10 * np.log10(1000*(avg_amp_Watt))
     
     # Add the extracted data as a tuple (temperature, average amplitude in dBm and Vrms, and error in Vrms) to data_list
-    data_list.append((indep_var, avg_amp_dBm, avg_amp_Vrms, error_Vrms))
+    data_list.append((indep_var, avg_amp_dBm, avg_amp_Watt, error_Watt))
 
 
 # Sort data_list by temperature (the first element of the tuple)
 sorted_data_list = sorted(data_list, key=lambda x: x[0])
 x_list = np.array(sorted_data_list)[:,0] * rescale_factor
 avg_amp_dBm_list = np.array(sorted_data_list)[:,1]
-avg_amp_Vrms_list = np.array(sorted_data_list)[:,2]
-error_Vrms_list = np.array(sorted_data_list)[:,3]
+avg_amp_Watt_list = np.array(sorted_data_list)[:,2]
+error_Watt_list = np.array(sorted_data_list)[:,3]
 
 if save_data == True:
     # save data in HDF5 format
@@ -76,15 +76,15 @@ if save_data == True:
     )
 
     lStep = [dict(name=indep_var_label, unit=indep_var_unit, values=x_list)]
-    lLog = [dict(name='Power at SPA', unit='dBm', vector=False,),
-            dict(name='Voltage at SPA', unit='V', vector=False,),
-            dict(name='error', unit='V', vector=False,),]
+    lLog = [dict(name='Power (dBm) at SPA', unit='dBm', vector=False,),
+            dict(name='Power (W) at SPA', unit='W', vector=False,),
+            dict(name='error', unit='W', vector=False,),]
     
     f = Labber.createLogFile_ForData(exp_name, lLog, lStep)
 
-    data = {'Power at SPA': avg_amp_dBm_list,
-            'Voltage at SPA': avg_amp_Vrms_list,
-            'error': error_Vrms_list}
+    data = {'Power (dBm) at SPA': avg_amp_dBm_list,
+            'Power (W) at SPA': avg_amp_Watt_list,
+            'error': error_Watt_list}
     f.addEntry(data)
 else:
     # plot data using pyplot
@@ -92,5 +92,12 @@ else:
     plt.scatter(x_list, avg_amp_dBm_list, color='blue')
     plt.xlabel(indep_var_label + " [" + indep_var_unit+ "]")
     plt.ylabel('Power at SPA [dBm]')
+    plt.grid(True)
+    plt.show()
+
+    plt.figure(figsize=(8, 6))
+    plt.scatter(x_list, avg_amp_Watt_list, color='blue')
+    plt.xlabel(indep_var_label + " [" + indep_var_unit+ "]")
+    plt.ylabel('Power at SPA [W]')
     plt.grid(True)
     plt.show()
